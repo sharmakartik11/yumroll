30f01a71de7f2c65f40dfb66fc1a9fe10d4082af
sharmakartik11 <sharma.kartik1@northeastern.edu>
2025-05-10 22:20:39 -0500

    separation of code

--- api/db.js
+++ api/db.js
@@ -1,10 +1,8 @@
-const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const app = express();
-app.use(express.json()); // Parse JSON body
+const uri =
+  "mongodb+srv://admin:letsyumrollit@yumroll.5utjiyy.mongodb.net/?retryWrites=true&w=majority&appName=YumRoll";
 
-const uri = "mongodb+srv://admin:letsyumrollit@yumroll.5utjiyy.mongodb.net/?retryWrites=true&w=majority&appName=YumRoll";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,30 +19,8 @@ async function connectDB() {
     console.error("MongoDB connection error:", err);
   }
 }
-connectDB();
 
-app.post("/api/register", async (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) {
-      return res.status(400).json({ error: "Username and password are required" });
-    }
-  
-    try {
-      const db = client.db("yumroll"); // Replace with your actual DB name
-      const users = db.collection("users");
-  
-      const result = await users.insertOne({ _id: username, password });
-      res.status(200).json({ message: "User registered", username: result.insertedId });
-    } catch (err) {
-      if (err.code === 11000) {
-        res.status(409).json({ error: "Username already exists" });
-      } else {
-        console.error("Insert failed:", err);
-        res.status(500).json({ error: "Internal server error" });
-      }
-    }
-  });
-
-const PORT = 5050;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+module.exports = {
+  connectDB,
+  client,
+};
--- api/server.js
+++ api/server.js
@@ -2,12 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 5050;
+const { connectDB, client } = require("./db");
+
+//connect to database
+connectDB();
 
 // Enable CORS for all origins
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/app", (req, res) => {
   res.status(200).json({ message: "Success from backend!" });
+});
+
+app.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  try {
+    const db = client.db("yumroll"); // Replace with your actual DB name
+    const users = db.collection("users");
+
+    const result = await users.insertOne({ _id: username, password });
+    res
+      .status(200)
+      .json({ message: "User registered", username: result.insertedId });
+  } catch (err) {
+    if (err.code === 11000) {
+      res.status(409).json({ error: "Username already exists" });
+    } else {
+      console.error("Insert failed:", err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 });
 
 app.listen(PORT, () => {
