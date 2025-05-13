92f71ad7c3d2a651e08b4ee1baf798023aa24578
sharmakartik11 <sharma.kartik1@northeastern.edu>
2025-05-12 23:17:29 -0500

    bcryptjs instead of bcrypt

--- yumroll-api/api/index.js
+++ yumroll-api/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const serverless = require("serverless-http");
 const jwt = require("jsonwebtoken");
 const app = express();
--- yumroll-api/db.js
+++ yumroll-api/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected");
+    console.log("Connected to DB:", mongoose.connection.name);
   } catch (err) {
     console.error(err.message);
     process.exit(1);
