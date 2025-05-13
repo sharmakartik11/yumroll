dd1ef4b214f4b8f2b7ff23cb66add5f72036af02
sharmakartik11 <sharma.kartik1@northeastern.edu>
2025-05-12 23:25:36 -0500

    remove app.listen

--- yumroll-api/api/index.js
+++ yumroll-api/api/index.js
@@ -3,15 +3,12 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const serverless = require("serverless-http");
 const jwt = require("jsonwebtoken");
-const app = express();
-const PORT = 5050;
 const connectDB = require("../db");
 const User = require("../models/User");
 
-//connect to database
+const app = express();
 connectDB();
 
-// Enable CORS for all origins
 app.use(cors());
 app.use(express.json());
 
@@ -19,53 +16,41 @@ app.get("/api/app", (req, res) => {
   res.status(200).json({ message: "Success from backend!" });
 });
 
-//signup API
 app.post("/signup", async (req, res) => {
   const { username, password } = req.body;
-
   try {
     let user = await User.findById(username);
     if (user) return res.status(400).json({ msg: "User already exists" });
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
+    const hashedPassword = await bcrypt.hash(password, 10);
     user = new User({ _id: username, password: hashedPassword });
     await user.save();
 
     res.status(201).json({ msg: "User registered successfully" });
   } catch (err) {
-    console.error(err.message);
+    console.error("Signup error:", err.message);
     res.status(500).send("Server error");
   }
 });
 
-//login API
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-
   try {
-    // Find user in db
     const user = await User.findById(username);
-    if (!user) {
-      return res.status(400).json({ msg: "Invalid username or password" });
-    }
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
 
-    // Compare hashed passwords
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid username or password" });
-    }
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-    res.status(200).json({ msg: "Login successful" });
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
   } catch (err) {
-    console.error(err.message);
+    console.error("Login error:", err.message);
     res.status(500).send("Server error");
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports.handler = serverless(app);
