7e1b985f9d23b0e31dc0f4fa2955a7998fca10b7
sharmakartik11 <sharma.kartik1@northeastern.edu>
2025-05-15 21:43:09 -0500

    added db.js basic

--- yumroll-api/index.js
+++ yumroll-api/index.js
@@ -1,41 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+require("dotenv").config();
 
 const app = express();
 const PORT = 5050;
 
+(async () => {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error("Fatal DB connection error");
+  }
+})();
+
 app.use(cors());
 app.use(express.json()); // Add this middleware to parse JSON payloads
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Yumroll API!');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Welcome to the Yumroll API!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-}
-);
+});
 
-app.post('/api/signup', (req, res) => {
+app.post("/api/signup", (req, res) => {
   const { username, password } = req.body;
 
   // Simulate a successful signup
   if (username && password) {
-    res.status(200).json({ message: 'Signup successful!' });
+    res.status(200).json({ message: "Signup successful!" });
   } else {
-    res.status(400).json({ error: 'Invalid input' });
+    res.status(400).json({ error: "Invalid input" });
   }
-}
-);
-app.post('/api/login', (req, res) => {
+});
+app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
   // Simulate a successful login
-  if (username === 'testuser' && password === 'password') {
-    res.status(200).json({ message: 'Login successful!' });
+  if (username === "testuser" && password === "password") {
+    res.status(200).json({ message: "Login successful!" });
   } else {
-    res.status(401).json({ error: 'Invalid credentials' });
+    res.status(401).json({ error: "Invalid credentials" });
   }
-}
-);
+});
